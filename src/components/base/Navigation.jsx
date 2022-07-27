f03189af61dbcf5d89d78b32845818a171ee2a6d
Kaushik-93 <k24561323@gmail.com>
2022-07-27 22:32:25 +0530

    Navbar

--- src/components/base/Navigation.jsx
+++ src/components/base/Navigation.jsx
@@ -1,80 +1,84 @@
-import React, { useState, useEffect } from "react";
-
-import { Container, Row, Col, Nav, Navbar } from "react-bootstrap";
-import { NavLink, useLocation } from "react-router-dom";
-
-import Data from "../../data";
-import Lifters from "../../images/Lifterv4.png";
-
+import React, { useState } from "react";
+// import styled from "styled-components";
+import logo from "../../images/Lifterv4.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
 const Navigation = () => {
-
-    const [location, setLocation] = useState();
-
-    useEffect(() => {
-        const loc = (window.location.href).split(Data.baseUrl);
-
-        if(loc[1] === "/contact") {
-            setLocation("contact");
-        } else if(loc[1] === "/courses") {
-            setLocation("courses");
-        } else if(loc[1] === "/about") {
-            setLocation("about");
-        } else if(loc[1] === "/") {
-            setLocation("home");
-        } else {
-            setLocation("")
-        }
-
-    }, []);
-    
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const MenuItems = () => {
     return (
-        <nav>
-            <Container>
-                <Row>
-                    <Col>
-                        <Row>
-                            <Col>
-                                <img className="logo" src={Lifters} alt="Lifters" />
-                            </Col>
-                            <Col>
-                                <h5 className="tittle">Lifter</h5>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col></Col>
-                    
-                    <Col><NavLink to="/" className={ location === "home" ? "navlinkActive" : "navlink" } >Home</NavLink></Col>
-                    <Col><NavLink to="/courses"  className={ location === "courses" ? "navlinkActive" : "navlink" } >Courses</NavLink></Col>
-                    <Col><NavLink to="/about"  className={ location === "about" ? "navlinkActive" : "navlink" }>About</NavLink></Col>
-                    <Col><NavLink to="/contact"  className={ location === "contact" ? "navlinkActive" : "navlink" }>Contact</NavLink></Col>
-                    <Col></Col>
-                    <Col>
-                        <Row>                    
-                            <b>sbaaala@gmail.com</b>
-                        </Row>
-                        <Row>                    
-                            <b>+91 90952 05004</b>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
-        </nav>
+      <div className="NavBar_MenuItems">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Courses</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
 
-        // <nav>
-        //     <Navbar bg="dark" variant="dark">
-        //         <Container>
-        //             <img className="logo" src={Lifters} alt="Lifters" />
-        //             <Navbar.Brand href="/" className="title">Lifters</Navbar.Brand>
-        //             <Nav className="me-auto">
-        //                 <Nav.Link href="/">Home</Nav.Link>
-        //                 <Nav.Link href="/courses">Courses</Nav.Link>
-        //                 <Nav.Link href="/about">About</Nav.Link>
-        //                 <Nav.Link href="/contact">Contact</Nav.Link>
-        //             </Nav>
-        //         </Container>
-        //     </Navbar>
-        // </nav>
-    )
-    }
+  return (
+    <>
+      <div className="NavBar">
+        <div className="NavBar_Logo">
+          <img src={logo} alt="logo"></img>
+          <span className="LogoName">Lifter</span>
+        </div>
 
+        <div className="NavBar_Links">
+          <ul>
+            <li className="active">
+              <a href="/">Home</a>
+            </li>
+            <div class="dropdown">
+              <li>
+                <a href="/">Courses</a>
+              </li>
+              <div class="dropdown-content">
+                <a href="#">Java Development</a>
+                <a href="#">Web Development</a>
+                <a href="#">Automation</a>
+              </div>
+            </div>
+
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <p>
+                sbaaala@gmail.com
+                <br />
+                +91 90952 05004
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="menu_icon">
+          {isNavOpen ? (
+            ({ MenuItems }, (<MdClose onClick={() => setIsNavOpen(false)} />))
+          ) : (
+            <GiHamburgerMenu
+              onClick={(e) => {
+                e.stopPropagation();
+                setIsNavOpen(true);
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 export default Navigation;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,33 @@
 import Navbar from "../components/base/Navigation";
-
+import img1 from "../images/HomePage.jpg";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const HomePage = () => (
-    <div>
-        <Navbar />
-        <h1>Home Page!</h1>
+  <div>
+    <Navbar />
+    <div className="Home_page1">
+      <div className="Homepage_illus">
+        <img src={img1} alt="img1"></img>
+      </div>
+      <div className="Homepage1_content">
+        <p>
+          Upgrade your skills
+          <br /> to require industry <br />
+          standards
+        </p>
+        <div className="getstarted_btn">
+          <button className="btn">
+            Get Started <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
+      {/* <div className="getstarted_btn">
+        <button className="btn">
+          Get Started <AiOutlineArrowRight />
+        </button>
+      </div> */}
     </div>
+  </div>
 );
 
 export default HomePage;

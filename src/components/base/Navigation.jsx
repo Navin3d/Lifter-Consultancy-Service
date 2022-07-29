d2e0b92688c9439a81d1772281ee2f13c98b85d0
Navin3d <smnavin65@mail.com>
2022-07-29 21:27:42 +0530

    fixes

--- src/components/base/Navigation.jsx
+++ src/components/base/Navigation.jsx
@@ -1,4 +1,7 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
+import { NavLink } from 'react-bootstrap';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -13,10 +16,35 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+import Lifter from "../../images/Lifterv4.png";
+import Data from "../../data";
+
+
+const pages = [
+  {
+    name: 'home',
+    url: "/"
+  },
+  {
+    name: 'courses', 
+    url: "/courses",
+  },
+  {
+    name:'contact',
+    url: "/contact"
+  },
+  {
+    name: 'about',
+    url: "/about"
+  }
+];
+
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
+
+  const navigate = useNavigate();
+
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -35,11 +63,33 @@ const ResponsiveAppBar = () => {
     setAnchorElUser(null);
   };
 
+
+  const [location, setLocation] = useState();
+
+    useEffect(() => {
+        const loc = (window.location.href).split(Data.baseUrl);
+
+        if(loc[1] === "/contact") {
+            setLocation("contact");
+        } else if(loc[1] === "/courses") {
+            setLocation("courses");
+        } else if(loc[1] === "/about") {
+            setLocation("about");
+        } else if(loc[1] === "/") {
+            setLocation("home");
+        } else {
+            setLocation("")
+        }
+
+    }, []);
+  
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <img src={Lifter} alt="lifter logo" style={{ maxWidth: "45px" }} />&nbsp;
+          {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +105,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Lifters
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -88,8 +138,8 @@ const ResponsiveAppBar = () => {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                <MenuItem key={page} className={ (location === page.name) && "active-link" } onClick={() => { navigate(page.url) }}>
+                  <Typography textAlign="center" ><NavLink to={`${page.url}`}>{page.name}</NavLink></Typography>
                 </MenuItem>
               ))}
             </Menu>
@@ -111,16 +161,17 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Lifters
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
+                className={ (location == page.name) ? "summa" : "active-link" }
                 key={page}
-                onClick={handleCloseNavMenu}
+                onClick={() => { navigate(page.url) }}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                {page}
+                <NavLink to={`${page.url}`}>{page.name}</NavLink>
               </Button>
             ))}
           </Box>
